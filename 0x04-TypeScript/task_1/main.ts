0f3d7739072bccad8e544a873c4a6c136736da18
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-07-12 09:21:24 +0100

    feat: setup task_1 folder with config files and define Teacher interface with strict and dynamic attributes

---
+++ 0x04-TypeScript/task_1/main.ts
@@ -0,0 +1,19 @@
+interface Teacher {
+  firstName: string;
+  fullTimeEmployee: boolean;
+  lastName: string;
+  age: number;
+  location: string;
+  contract: boolean;
+}
+
+const teacher3: Teacher = {
+  firstName: "Eleanya",
+  fullTimeEmployee: false,
+  lastName: "Onuma",
+  age: 25,
+  location: "Nigeria",
+  contract: false,
+};
+
+console.log(teacher3);
