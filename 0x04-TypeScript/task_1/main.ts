16ef1fc302e4be57e9740a633e34debe45bda7cd
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-07-12 17:49:49 +0100

    refactor: improve code structure in main.js

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -7,6 +7,14 @@ interface Teacher {
   contract: boolean;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction { 
+  (firstName: string, lastName: string): string;
+}
+
 const teacher3: Teacher = {
   firstName: "Eleanya",
   fullTimeEmployee: false,
@@ -17,3 +25,39 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: "John",
+  fullTimeEmployee: true,
+  lastName: "Doe",
+  age: 40,
+  location: "USA",
+  contract: true,
+  numberOfReports: 17,
+}
+
+function printTeacher(firstName: string, lastName: string): string { 
+  return `${firstName.charAt(0)} ${lastName}`;
+}
+console.log(printTeacher(teacher3.firstName, teacher3.lastName));
+console.log(printTeacher(director1.firstName, director1.lastName));
+
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+  workOnHomeWork: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.workOnHomeWork = "Currently working";
+  }
+
+  displayName(): string {
+    return `${this.firstName}`;
+  }
+}
+
+const student1 = new StudentClass("Jane", "Doe");
+console.log(student1.displayName());
